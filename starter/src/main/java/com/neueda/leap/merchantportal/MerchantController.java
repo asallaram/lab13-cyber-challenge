4086ea8ec5e5690a10f1c93570c30af6c6866de7
asallaram <aneeshsallaram@gmail.com>
2026-08-27 17:59:26 +0000

    Fixed A01 issue in MerchantController by adding check to ensure merchant and user requesting data have matching ids.

--- starter/src/main/java/com/neueda/leap/merchantportal/MerchantController.java
+++ starter/src/main/java/com/neueda/leap/merchantportal/MerchantController.java
@@ -9,9 +9,21 @@ public class MerchantController {
     @Autowired
     private PayoutRepository payoutRepository;
 
-    @GetMapping("/api/payouts/{payoutId}")
-    public PayoutRequest getPayout(@PathVariable Long payoutId) {
-        return payoutRepository.findById(payoutId)
+    /*This an OWASP A01: Broken Access Control
+    because there is no check to ensure that the
+    merchant requesting data is the owner of the 
+    payout. Fixed by adding a check to ensure
+    the merchant is indeed owner of payout.
+    */
+    @GetMapping("/api/merchants/{merchantId}/payouts/{payoutId}")
+    public PayoutRequest getPayout(@PathVariable Long merchantId, @PathVariable Long payoutId) {
+        PayoutRequest payout = payoutRepository.findById(payoutId)
                 .orElseThrow(() -> new RuntimeException("Payout not found"));
+
+        if (!payout.getMerchantId().equals(merchantId)) {
+            throw new RuntimeException("Not authorized to view this payout");
+        }
+
+        return payout;
     }
 }
